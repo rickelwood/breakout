4465bc6b761fc20a7d0e410496cfa8fab60e29a4
Rick Elwood <relwood@amazon.com>
2018-11-28 22:35:33 -0800

    Separate paddle, ball and blocks on to their own canvas

--- breakout.js
+++ breakout.js
@@ -1,7 +1,9 @@
 var leftKey = false; // True when left arrow is down
 var rightKey = false; // True when right arrow is down
 var paddleXPos = 300; // Position of paddle x axis
-var context = document.getElementById("myCanvas").getContext("2d");
+var blockContext = document.getElementById("blockCanvas").getContext("2d");
+var paddleContext = document.getElementById("paddleCanvas").getContext("2d");
+var ballContext = document.getElementById("ballCanvas").getContext("2d");
 var colors = ['rgb(200,0,0)','rgb(220,200,0)','rgb(100,200,0)','rgb(50,100,200)', 'rgb(250,100,250)']
 var ball = {
 	x: 5,
@@ -13,8 +15,6 @@ var ball = {
 };
 
 function draw() {	
-	// Clear the canvas
-	context.clearRect(0, 0, 600, 600);
 
 	// Draw the Ball
 	drawBall();	
@@ -22,21 +22,31 @@ function draw() {
 	// Draw paddle
 	drawPaddle();
 
-	// Draw all the blocks
-	for (y = 0; y < 4; y += 1) {
-	    for (x = 0; x < 10; x +=1) {
-
-	        // Draw a block
-	        context.fillStyle = colors[y]; 
-	        context.fillRect(15 + x * 57, 15 + y * 32, 50, 25); 
-	    }
-	}
+	// Draw blocks
+	drawBlocks();
 
 	// Draw the frame
 	window.requestAnimationFrame(draw);
 }
 
+function drawBlocks() {
+	// Clear the canvas
+	blockContext.clearRect(0, 0, 600, 600);
+
+	// Draw all the blocks
+	for (y = 0; y < 4; y += 1) {
+		for (x = 0; x < 10; x +=1) {
+
+			// Draw a block
+			blockContext.fillStyle = colors[y]; 
+			blockContext.fillRect(15 + x * 57, 15 + y * 32, 50, 25); 
+		}
+	}
+}
+
 function drawBall() {
+	// Clear the canvas
+	ballContext.clearRect(0, 0, 600, 600);
 	
 	// Reverse x direction if we hit wall
 	if (ball.x < 5 || ball.x > 595) {
@@ -53,11 +63,14 @@ function drawBall() {
 	ball.y = ball.y + ball.vy;
 
 	// Draw the Ball
-	context.fillStyle = 'rgb(0,0,200)'; 
-	context.fillRect(ball.x, ball.y, ball.h, ball.w); 
+	ballContext.fillStyle = 'rgb(0,0,200)'; 
+	ballContext.fillRect(ball.x, ball.y, ball.h, ball.w); 
 }
 
 function drawPaddle() {
+	// Clear the canvas
+	paddleContext.clearRect(0, 0, 600, 600);
+
 	// move paddle left if left is true
 	if (leftKey == true && paddleXPos >= 4){
 		paddleXPos = paddleXPos - 5; // Move paddle left 
@@ -69,8 +82,8 @@ function drawPaddle() {
 	}
 
 	// Draw the Paddle
-	context.fillStyle = 'rgb(50,200,50)'; 
-	context.fillRect(paddleXPos, 580, 100, 15); 
+	paddleContext.fillStyle = 'rgb(50,200,50)'; 
+	paddleContext.fillRect(paddleXPos, 580, 100, 15); 
 
 }
 
