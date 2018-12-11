d201b8d34e46c9db84c3d41740b16f90c9cc8aca
Rick Elwood <relwood@amazon.com>
2018-12-11 00:43:40 -0800

    Add basic collisions and levels

--- breakout.js
+++ breakout.js
@@ -6,13 +6,34 @@ var paddleContext = document.getElementById("paddleCanvas").getContext("2d");
 var ballContext = document.getElementById("ballCanvas").getContext("2d");
 var colors = ['rgb(200,0,0)','rgb(220,200,0)','rgb(100,200,0)','rgb(50,100,200)', 'rgb(250,100,250)']
 var ball = {
-	x: 5,
-	y: 500,
+	x: 250,
+	y: 270,
 	h: 10,
 	w: 10,
 	vx: 2,
 	vy: 5
 };
+var level = 1;
+
+var levels = [[
+	1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+	1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+	1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+	1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+	1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+	1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+	1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+	1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+],[
+	1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+	1, 1, 1, 2, 1, 1, 2, 1, 1, 1,
+	1, 1, 1, 2, 1, 1, 2, 1, 1, 1,
+	1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+	1, 1, 2, 1, 1, 1, 1, 2, 1, 1,
+	1, 1, 1, 2, 1, 1, 2, 1, 1, 1,
+	1, 1, 1, 1, 2, 2, 1, 1, 1, 1,
+	1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+]];
 
 function draw() {	
 
@@ -34,12 +55,15 @@ function drawBlocks() {
 	blockContext.clearRect(0, 0, 600, 600);
 
 	// Draw all the blocks
-	for (y = 0; y < 4; y += 1) {
+	for (y = 0, i = 0; y < 8; y += 1) {
 		for (x = 0; x < 10; x +=1) {
 
 			// Draw a block
-			blockContext.fillStyle = colors[y]; 
-			blockContext.fillRect(15 + x * 57, 15 + y * 32, 50, 25); 
+			if (levels[level][i] > 0) {
+				blockContext.fillStyle = colors[levels[level][i]-1]; 
+				blockContext.fillRect(15 + x * 57, 15 + y * 32, 50, 25); 
+			}
+			i += 1;
 		}
 	}
 }
@@ -47,6 +71,10 @@ function drawBlocks() {
 function drawBall() {
 	// Clear the canvas
 	ballContext.clearRect(0, 0, 600, 600);
+	
+	// Check collisions
+	checkBrickCollision();
+	checkPaddleCollision()
 	
 	// Reverse x direction if we hit wall
 	if (ball.x < 5 || ball.x > 595) {
@@ -65,6 +93,41 @@ function drawBall() {
 	// Draw the Ball
 	ballContext.fillStyle = 'rgb(0,0,200)'; 
 	ballContext.fillRect(ball.x, ball.y, ball.h, ball.w); 
+}
+
+function checkBrickCollision() {
+	// Take advantage of the grid
+	// Did it hit a brick?
+
+	// If we are below the bricks then no
+	if (ball.y > 265) {
+		return false;
+	} else {
+		ball.vy *= -1;
+		return true;
+	}
+}
+
+function checkPaddleCollision() {
+	// Take advantage of the grid
+	// Did it hit a brick?
+
+	// If we are below the bricks then no
+	if (ball.y < 580 - ball.vy) {
+		return false;
+	} else if (ball.x >= paddleXPos && ball.x <= paddleXPos + 90) {
+		ball.vy *= -1;
+		return true;
+	} else {
+		ball = {
+			x: 250,
+			y: 270,
+			h: 10,
+			w: 10,
+			vx: 2,
+			vy: 5
+		};
+	}
 }
 
 function drawPaddle() {
